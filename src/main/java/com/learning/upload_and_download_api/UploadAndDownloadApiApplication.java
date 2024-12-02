0d56d6fce5f9c83f3ceb544e170e1a0eb6d3077b
Joilson Capemba <joilsoncapemba@gmail.com>
2024-12-02 11:17:34 +0100

    first commit

---
+++ src/main/java/com/learning/upload_and_download_api/UploadAndDownloadApiApplication.java
@@ -0,0 +1,13 @@
+package com.learning.upload_and_download_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadAndDownloadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadAndDownloadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/upload_and_download_api/UploadAndDownloadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.upload_and_download_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadAndDownloadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
